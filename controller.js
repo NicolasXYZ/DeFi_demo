1d5e5961a5b63aa7cd07aaf5f9ba09b76f9871a1
Nicolas Zhang <nicolaszhang@C02DF5XAMD6M.tld>
2021-06-10 23:13:52 +0200

    added multiple accounts

--- controller.js
+++ controller.js
@@ -7,108 +7,124 @@ const { wrap } = require('@awaitjs/express');
 
 module.exports = function (app) {
 
+    app.route('/UserSelection/:userID').get((req, res) => {
+        if (isNaN(req.params.userID)||(req.params.userID<0)||(req.params.userID>9)) {
+            return res.sendStatus(400);
+        }
+        app.locals.user = req.params.userID
+        console.dir((app.locals.user));
+        return res.sendStatus(200);
+    });
+        
     var service = require('./service.js');
 
-/*
+    app.route('/checkAccount/ETHBalance').get(async (req, res) => {
+        await service.startAndCheckEther(parseInt(app.locals.user)).then((result) => {
+            return res.send(result);
+        }).catch((error) => {
+            console.log(error)
+            return res.sendStatus(400);
+        });
+    });
 
-    app.route('/supplyRateDAI')
-        .get(service.supplyRateDAI)
+    app.route('/supplyETH/:amount').get(async (req, res) => {
+        if (isNaN(req.params.amount)) {
+            return res.sendStatus(400);
+        }
+        await service.SupplyETH(parseInt(app.locals.user)，req.params.amount).then((result) => {
+            return res.sendStatus(200);
+        }).catch((error) => {
+            return res.sendStatus(400);
+        });
+    });
+
+    /*
+    
+        app.route('/supplyRateDAI')
+            .get(service.supplyRateDAI)
+    
+    
+        app.route('/supplyRateETH')
+            .get(service.borrowRateETH)
+    
+        app.route('/exchangeRatecDAIcETH')
+            .get(service.exchangeRatecDAIcETH)
+    
+        app.route('/exchangeRateDAIcDAI')
+            .get(service.exchangeRateDAIcDAI)
+    
+        app.route('/redeemETH/:amount')
+            .get(service.redeemETH)
+    
+        app.route('/redeemDAI/:amount')
+            .get(service.redeemDAI)
+    
+        app.route('/borrowETH/:amount').get(async (req, res) => {
+            if (isNaN(req.params.amount)) {
+                return res.sendStatus(400);
+            }
+            await service.borrowETH(req.params.amount).then((result) => {
+                return res.sendStatus(200);
+            }).catch((error) => {
+                return res.sendStatus(400);
+            });
+        });
+    
+    
+        app.route('/repayETH/:amount').get(async (req, res) => {
+            if (isNaN(req.params.amount)) {
+                return res.sendStatus(400);
+            }
+            await service.repayETH(req.params.amount).then((result) => {
+                return res.sendStatus(200);
+            }).catch((error) => {
+                return res.sendStatus(400);
+            });
+        });
+    
+    
+        app.route('/repayDAI/:amount').get(async (req, res) => {
+            if (isNaN(req.params.amount)) {
+                return res.sendStatus(400);
+            }
+            await service.repayDAI(req.params.amount).then((result) => {
+                return res.sendStatus(200);
+            }).catch((error) => {
+                return res.sendStatus(400);
+            });
+        });
+        app.route('/checkAccount/cDAIBalance')
+            .get(service.checkcDAIBalance)
+    
+        app.route('/supplyDAI/:amount').get(async (req, res) => {
+            if (isNaN(req.params.amount)) {
+                return res.sendStatus(400);
+            }
+            await service.SupplyETH(req.params.amount).then((result) => {
+                return res.sendStatus(200);
+            }).catch((error) => {
+                return res.sendStatus(400);
+            });
+        });
+    */
+    ////////////////////////////////////////
 
 
-    app.route('/supplyRateETH')
+    app.route('/borrowRateETH')
         .get(service.borrowRateETH)
 
-    app.route('/exchangeRatecDAIcETH')
-        .get(service.exchangeRatecDAIcETH)
-
-    app.route('/exchangeRateDAIcDAI')
-        .get(service.exchangeRateDAIcDAI)
-
-    app.route('/redeemETH/:amount')
-        .get(service.redeemETH)
-
-    app.route('/redeemDAI/:amount')
-        .get(service.redeemDAI)
-
-    app.route('/borrowETH/:amount').get(async (req, res) => {
-        if (isNaN(req.params.amount)) {
-            return res.sendStatus(400);
-        }
-        await service.borrowETH(req.params.amount).then((result) => {
-            return res.sendStatus(200);
-        }).catch((error) => {
-            return res.sendStatus(400);
-        });
-    });
-
-
-    app.route('/repayETH/:amount').get(async (req, res) => {
-        if (isNaN(req.params.amount)) {
-            return res.sendStatus(400);
-        }
-        await service.repayETH(req.params.amount).then((result) => {
-            return res.sendStatus(200);
-        }).catch((error) => {
-            return res.sendStatus(400);
-        });
-    });
-
-
-    app.route('/repayDAI/:amount').get(async (req, res) => {
-        if (isNaN(req.params.amount)) {
-            return res.sendStatus(400);
-        }
-        await service.repayDAI(req.params.amount).then((result) => {
-            return res.sendStatus(200);
-        }).catch((error) => {
-            return res.sendStatus(400);
-        });
-    });
-    app.route('/checkAccount/cDAIBalance')
-        .get(service.checkcDAIBalance)
-
-    app.route('/supplyDAI/:amount').get(async (req, res) => {
-        if (isNaN(req.params.amount)) {
-            return res.sendStatus(400);
-        }
-        await service.SupplyETH(req.params.amount).then((result) => {
-            return res.sendStatus(200);
-        }).catch((error) => {
-            return res.sendStatus(400);
-        });
-    });
-*/
-////////////////////////////////////////
-
-
-app.route('/borrowRateETH')
-.get(service.borrowRateETH)
-
-app.route('/borrowRateDAI')
-.get(service.borrowRateDAI)
+    app.route('/borrowRateDAI')
+        .get(service.borrowRateDAI)
 
     app.route('/exchangeRateETHcETH')
         .get(service.exchangeRateETHcETH)
 
-    app.route('/checkAccount/ETHBalance')
-        .get(service.startAndCheckEther)
 
     app.route('/checkAccount/cETHBalance')
         .get(service.checkCTokenBalance)
 
     app.route('/startGanache')
         .get(service.startGanache)
-
-    app.route('/supplyETH/:amount').get(async (req, res) => {
-        if (isNaN(req.params.amount)) {
-            return res.sendStatus(400);
-        }
-        await service.SupplyETH(req.params.amount).then((result) => {
-            return res.sendStatus(200);
-        }).catch((error) => {
-            return res.sendStatus(400);
-        });
-    });
 
     app.route('/checkCTokenBalance')
         .get(service.checkAccountcETHBalance)
--- service.js
+++ service.js
@@ -8,6 +8,8 @@ const web3 = provider;
 const privateKey = '0x7fd0583acffe10c073f6fbc4bdf9405d821ba25085928aa6409fe43b76913971';
 web3.eth.accounts.wallet.add(privateKey);
 const myWalletAddress = web3.eth.accounts.wallet[0].address;
+var AccountList = ['0xa3957d49125150BF1155a57eaF259d1604069EE2', '0xE695c30D25cDcDEc4A8a6340F678296D0F1E3863','0x0a57189D0114e7Bb93dcA4B1E92ceA9f9148d616','0x71076954757B1fFec9a8C7545e1006c73d8A94e2','0x2f9968aB365F8136860eBF6aCFDaD88C09E255D5','0xc1056eEC65e63Fe951E18184C0baC0E8D272569b','0xDc8B64E3103cc270fa7Ce7108036e7883f6591D5','0xB654c438F99BD3075B117e153238e96C0C40b332','0x012B6F2C38976bEd23FAE5d764214c665Ff9A1fA','0x160Db28Ffd9D7ccC46907D18b309dB1Ef237db8a'];
+var privateKeyList = ['0x7fd0583acffe10c073f6fbc4bdf9405d821ba25085928aa6409fe43b76913971','0x92bfb3ec4e7b17a7bdc5b0e2b68b1e664bdd2a7c9801e3fb69c59c82cfdaf83a','0xaf861ebb5caa1375b335d875e976b35a7326287d4f6dce7e5a0c2490d8a536df','0x0eaab927269f9f04fd5081a5b5191283758de7fce8d8e45b21d8ea3a5818b41a','0x47e4b9f9e87833082b634094aecb58e09f79edc191f2105e3fae983f5f293ee8','0x821aa7dd294a0727d50f3e7df3856e2600c9ac579ee507d75a8e3b53a05e69f5','0xce5b2bda4d719eb9ed98f493af18672541a54cc5acabd45a50998d646041c8d8','0x15aeb58ac94c1a8824a4d74948fb0d49b59ab9a9af82662ca5779bd61264e96b','0x2356f51affff76eadbca57b5d9c8cbd696478ce217a44b13b80e42d402f8ca65','0x68d87d3961740512a33da225ec8b9781baef19a5e15339dcd7477bc6c487e70f'];
 //var ethers = require('ethers');
 //const { ethers } = require("ethers")
 //const Ganache = require("ganache-core")
@@ -90,24 +92,15 @@ exports.startGanache = async function (req, res) {
   return res;
 };
 
-exports.startAndCheckEther = async function (req, res) {
+exports.startAndCheckEther = async function (user, res) {
   //const provider = new ethers.providers.Web3Provider(ganache.provider())
-  web3.eth.getBalance("0xa3957d49125150BF1155a57eaF259d1604069EE2", function (err, result) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(web3.utils.fromWei(result, "ether") + " ETH")
-    }
-
-    var response = {
-      "text": "balance in Ether in our wallet: " + web3.utils.fromWei(result, "ether")
-    };
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(response));
-  })
-
+  //web3.eth.getBalance("0xa3957d49125150BF1155a57eaF259d1604069EE2", function (err, result) {
+    let ETHBalance = await web3.eth.getBalance(AccountList[user]);
+  var response = {
+    "text": "balance in Ether in our wallet: " + web3.utils.fromWei(ETHBalance, "ether")
+  };
+  res = JSON.stringify(response);
+  console.log(res)
   return res;
 };
 
